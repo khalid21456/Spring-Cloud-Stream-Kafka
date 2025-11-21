4b23d847a64fa3121982924afd49b058db0f9403
khalid21456 <khalidedaoudi8@gmail.com>
2025-11-21 04:48:10 +0100

    changes

--- src/main/java/com/example/kafka/web/PageEventRestController.java
+++ src/main/java/com/example/kafka/web/PageEventRestController.java
@@ -1,4 +1,16 @@
 package com.example.kafka.web;
 
+
+import com.example.kafka.entities.PageEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PageEventRestController {
+
+    @GetMapping("/publish/{topic}/{name}")
+    public PageEvent publish() {
+
+    }
+
 }
