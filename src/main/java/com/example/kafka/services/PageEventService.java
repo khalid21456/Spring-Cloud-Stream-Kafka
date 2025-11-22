129874231106bbee280c5281ca61533724abed74
khalid21456 <khalidedaoudi8@gmail.com>
2025-11-22 03:31:08 +0100

    consumer

---
+++ src/main/java/com/example/kafka/services/PageEventService.java
@@ -0,0 +1,21 @@
+package com.example.kafka.services;
+
+import com.example.kafka.entities.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Service
+public class PageEventService {
+
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input) -> {
+            System.out.println("**************************************");
+            System.out.println(input.toString());
+            System.out.println("**************************************");
+        };
+    }
+
+}
--- src/main/java/com/example/kafka/web/PageEventRestController.java
+++ src/main/java/com/example/kafka/web/PageEventRestController.java
@@ -2,15 +2,27 @@ package com.example.kafka.web;
 
 
 import com.example.kafka.entities.PageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class PageEventRestController {
 
-    @GetMapping("/publish/{topic}/{name}")
-    public PageEvent publish() {
+    @Autowired
+    private StreamBridge streamBridge;
 
+    @GetMapping("/publish/{topic}/{name}")
+    public PageEvent publish(@PathVariable String topic, @PathVariable String name) {
+        PageEvent pageEvent = new PageEvent(name,Math.random() > 0.5?"U1":"U2",new Date(),new Random().nextInt(9000));
+        streamBridge.send(topic,
+                pageEvent);
+        return pageEvent;
     }
 
 }
